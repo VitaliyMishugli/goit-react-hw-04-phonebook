4d70f9bce17a226f758a392be0a1cee8971c6910
Vitaliy Mishugli <vmishughli@gmail.com>
2023-02-28 21:13:09 +0000

    28.02.2023-3

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -8,8 +8,8 @@ export const ContactList = ({ visibleContacts, onDeleteContact}) => {
       {visibleContacts.map(({ name, id, number }) => (
         <li key={id} style={{ marginBottom: '10px' }}>
           <ContactItem name={name} id={id} number={number} onDeleteContact={ onDeleteContact} />
-          <span style={{marginRight: '10px'}}>{name} : {number}</span>
-          <button onClick={()=>onDeleteContact(id)}>Delete</button>
+          {/* <span style={{marginRight: '10px'}}>{name} : {number}</span>
+          <button onClick={()=>onDeleteContact(id)}>Delete</button> */}
         </li>))}
     </ul>
   )
